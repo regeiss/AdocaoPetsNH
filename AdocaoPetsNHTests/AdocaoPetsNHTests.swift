502f0ad73181c2db9acabea54bf27b4bb679eb6d
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2025-02-12 20:20:08 -0300

    Initial Commit

---
+++ AdocaoPetsNHTests/AdocaoPetsNHTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdocaoPetsNHTests.swift
+//  AdocaoPetsNHTests
+//
+//  Created by Roberto Edgar Geiss on 12/02/25.
+//
+
+import Testing
+@testable import AdocaoPetsNH
+
+struct AdocaoPetsNHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
